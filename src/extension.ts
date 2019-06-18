4246e90f6677e09120f2bb82ebd48a19e41d17c7
Ivan Ladyha <iladyha@xbsoftware.by>
2019-06-18 16:41:39 +0300

    minor fix

--- src/extension.ts
+++ src/extension.ts
@@ -11,8 +11,6 @@ export function activate(context: vscode.ExtensionContext) {
 	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
 	vscode.commands.registerCommand('nodeDependency.navigate', navigateToFile);
 
 }
