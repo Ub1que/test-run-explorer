8fdcb9c6f31991d190dfd3256c9e925ad314ecbe
Ivan Ladyha <iladyha@xbsoftware.by>
2019-07-09 17:11:49 +0300

    icons and collapse state for specs

--- src/Dependencies/Spec.ts
+++ src/Dependencies/Spec.ts
@@ -19,10 +19,60 @@ export default class Spec extends vscode.TreeItem {
 		return;
 	}
 
-	iconPath = {
-		light: path.join(__filename, '..','..', '..', 'resources', 'light', 'dependency.svg'),
-		dark: path.join(__filename, '..', '..', '..', 'resources', 'dark', 'dependency.svg')
-	};
+	get iconPath() {
+	
+		let overallFailed = [];
+		let overallSkipped = [];
+
+		function findFailed(object) {
+			if (object && Array.isArray(object)){
+
+				object.forEach(function (obj) {
+					return findFailed(obj);
+				});
+
+			} else if (object && typeof object === 'object'){
+				Object.keys(object).forEach(function (key) {
+
+					if (object[key] && Array.isArray(object[key])){
+						
+						object[key].forEach(function (obj) {
+							return findFailed(obj);
+						});
+
+					} else if (object[key] && typeof object[key] === 'object'){
+						return findFailed(object[key]);
+					}
+
+					if (key === 'fail' && object[key] === true) {
+						overallFailed.push(object.uuid);
+					} else if (key === 'state' && object[key] === 'skipped') {
+						overallSkipped.push(object.uuid);
+					}
+					
+				});
+			}
+		}
+
+		findFailed(this.entry);
+
+		if (overallFailed.length !== 0) {
+			return {
+				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg'),
+				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg')
+			};
+		} else if (overallSkipped.length !== 0) {
+			return {
+				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg'),
+				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg')
+			};
+		} else {
+			return {
+				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg'),
+				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg')
+			};
+		}
+	}
 
 	contextValue = 'dependency';
 
