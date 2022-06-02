f772849252bbbe52438881b5b5d0cb9643b7e9d1
Ivan Ladyha <evan.ladyha@gmail.com>
2022-06-02 20:57:43 +0300

    test-commit

--- src/Commands/navigate.ts
+++ src/Commands/navigate.ts
@@ -1,26 +1,34 @@
 import * as vscode from 'vscode';
 import codeParser from '../Parser/parser';
 
-export default function navigateToFile(testName){
-	const token = codeParser(testName);
-	
-	switch (true) {
-		case (token.length === 0):
-			vscode.window.showInformationMessage(`No tests found on name '${testName}'`);
-			break;
-	
-		case (token.length > 1):
-			vscode.window.showInformationMessage(`More than one test was found on name '${testName}'. Moveing to first`);
-
-		default:
-			vscode.workspace.openTextDocument(token[0].path).then(d=> {
-				vscode.window.showTextDocument(d).then(textEditor=>{
-					goTolLine(token[0].loc.start.line);           
-				});
-			});
-	}
-
+export default function navigate(location, path){
+	vscode.workspace.openTextDocument(path).then(d=> {
+		vscode.window.showTextDocument(d).then(textEditor=>{
+			goTolLine(location.line);           
+		});
+	});
 }
+
+// export default function navigateToFile(testName){
+// 	const token = codeParser(testName);
+	
+// 	switch (true) {
+// 		case (token.length === 0):
+// 			vscode.window.showInformationMessage(`No tests found on name '${testName}'`);
+// 			break;
+	
+// 		case (token.length > 1):
+// 			vscode.window.showInformationMessage(`More than one test was found on name '${testName}'. Moveing to first`);
+
+// 		default:
+// 			vscode.workspace.openTextDocument(token[0].path).then(d=> {
+// 				vscode.window.showTextDocument(d).then(textEditor=>{
+// 					goTolLine(token[0].loc.start.line);           
+// 				});
+// 			});
+// 	}
+
+// }
 
 function goTolLine(line: number):void {
 	var line = line===0 ? line : line-1;
--- src/Dependencies/Spec.ts
+++ src/Dependencies/Spec.ts
@@ -6,6 +6,7 @@ export default class Spec extends vscode.TreeItem {
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
 		public readonly entry: any,
 		public readonly fileName: any,
+		public readonly status: any,
 		public readonly command?: vscode.Command
 	) {
 		super(fileName, collapsibleState);
@@ -20,57 +21,22 @@ export default class Spec extends vscode.TreeItem {
 	}
 
 	get iconPath() {
-	
-		let overallFailed = [];
-		let overallSkipped = [];
-
-		function findFailed(object) {
-			if (object && Array.isArray(object)){
-
-				object.forEach(function (obj) {
-					return findFailed(obj);
-				});
-
-			} else if (object && typeof object === 'object'){
-				Object.keys(object).forEach(function (key) {
-
-					if (object[key] && Array.isArray(object[key])){
-						
-						object[key].forEach(function (obj) {
-							return findFailed(obj);
-						});
-
-					} else if (object[key] && typeof object[key] === 'object'){
-						return findFailed(object[key]);
-					}
-
-					if (key === 'fail' && object[key] === true) {
-						overallFailed.push(object.uuid);
-					} else if (key === 'state' && object[key] === 'skipped') {
-						overallSkipped.push(object.uuid);
-					}
-					
-				});
-			}
-		}
-
-		findFailed(this.entry);
-
-		if (overallFailed.length !== 0) {
-			return {
-				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg'),
-				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg')
-			};
-		} else if (overallSkipped.length !== 0) {
-			return {
-				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg'),
-				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg')
-			};
-		} else {
-			return {
-				light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg'),
-				dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg')
-			};
+		switch (this.status) {
+			case 'skipped':
+				return {
+					light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg'),
+					dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'skip.svg')
+				};
+			case 'failed':
+				return {
+					light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg'),
+					dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'fail.svg')
+				};
+			default:
+				return {
+					light: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg'),
+					dark: path.join(__filename, '..', '..', '..', 'resources', 'color', 'pass.svg')
+				};
 		}
 	}
 
